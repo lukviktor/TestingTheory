98646f145fdc974dfc51aed55a270e96d22538a6
viktor <lukashevviktor@gmail.com>
2023-05-15 02:35:18 +0300

    Добавил термины TDD, BDD, DDT, KDT в словарь Terminology

--- src/test/java/testing/theory/kulikov_322_26_03_2023/TerminologyTest.java
+++ src/test/java/testing/theory/kulikov_322_26_03_2023/TerminologyTest.java
@@ -1,8 +1,15 @@
 package testing.theory.kulikov_322_26_03_2023;
 
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import testing.theory.kulikov_322_26_03_2023.terminology.Terminology;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static testing.theory.kulikov_322_26_03_2023.terminology.ConstantDataTerminology.*;
 
 class TerminologyTest {
     Terminology terminology = new Terminology();
@@ -21,5 +28,25 @@ class TerminologyTest {
     @Test
     public void softwareTestingOutputTest() {
         terminology.softwareTestingOutput();
+        // перенаправляем вывод на ByteArrayOutputStream
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        // вызываем метод, который выводит результат на консоль
+        terminology.softwareTestingOutput();
+
+        // читаем сохраненный в outputStream вывод
+        String expectedOutput = String.join("\n",
+                SOFTWARE_TESTING_1,
+                SOFTWARE_TESTING_2,
+                SOFTWARE_TESTING_3,
+                ""
+        );
+        String actualOutput = outputStream.toString().replaceAll("\\r\\n", "\n");
+        assertEquals(expectedOutput, actualOutput);
     }
+
 }
+
+
+
